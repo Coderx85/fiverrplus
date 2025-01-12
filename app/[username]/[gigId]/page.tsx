935e93e55ae6f0c864d7a8eeea5b48242b86e92d
Coderx85 <work.priyanshu085@gmail.com>
2025-01-12 12:45:03 +0530

    Add Offers component and integrate it into GigPage for displaying available offers

--- app/[username]/[gigId]/page.tsx
+++ app/[username]/[gigId]/page.tsx
@@ -1,6 +1,10 @@
 "use client";
 
+import { useQuery } from "convex/react"
 import { Header } from "./_components/header"
+import { api } from "@/convex/_generated/api"
+import { Id } from "@/convex/_generated/dataModel"
+import { Offers } from "./_components/offers";
 
 interface PageProps {
     params: {
@@ -13,17 +17,22 @@ const GigPage = ({
     params
 }: PageProps) => {
     
-
     return (
         <div>
             <div className="flex flex-col sm:flex-row w-full sm:justify-center space-x-0 sm:space-x-3 lg:space-x-16">
                 <div className="w-full space-y-8">
                     <Header
-                        // 
-                        // editUrl={}
-                        // ownerId={}
+                        {}
+                        editUrl={}
+                        ownerId={}
                     />
+                    
                 </div>
+                <Offers
+                    offers={}
+                    sellerId={}
+                    editUrl={}
+                />
             </div>
         </div>
     )
